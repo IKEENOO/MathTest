005e1c706fb2e709c97c36e98eed13683d2665f3
IKEENOO <94290833+IKEENOO@users.noreply.github.com>
2023-04-02 18:39:50 +0300

    init: start test task

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let btnFinishTest = document.getElementById("finishTest");
+
+btnFinishTest.addEventListener("click", checkTest);
+
+function checkTest() {
+    let result = 0;
+
+    let answerOne = document.getElementById("questionOne").value;
+    if(answerOne === "4"){
+        result++;
+    }
+
+    let answerTwo = document.getElementById("questionTwo").value;
+    if(answerTwo === "12"){
+        result++;
+    }   
+
+    let answerThree = document.getElementById("questionThree").value;
+    if(answerThree === "2"){
+        result++;
+    }
+
+    alert("\nNumber of correct answers: " + result + "\n\nCongratulations!");
+}
